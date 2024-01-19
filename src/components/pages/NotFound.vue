580accf57a03c0a1623393ad4599cbf86a950991
torreslucs23 <torreslucs23@gmail.com>
2024-01-19 18:10:47 -0300

    added logout and add, home button on navigation

---
+++ src/components/pages/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>
+    Página não encontrada! Tente retornar para a página principal
+    <router-link to="/home">Home</router-link>
+  </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import TheNavigation from "./components/nav/TheNavigation.vue";
+
 const app = createApp(App);
+
+app.component("the-navigation", TheNavigation);
 
 app.use(router);
 
